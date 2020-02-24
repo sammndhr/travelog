f21475bc6d11ecd754a154cee4e0e3d0ee49f28a
mrinalini-m <mrinalini.manandhar@gmail.com>
2020-02-23 22:08:02 -0800

    Add @mdi/font

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,6 @@ import vuetify from '@/plugins/vuetify'
 import router from '@/router'
 import { store } from '@/store'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vue from 'vue'
 import VueMasonry from 'vue-masonry-css'
 
