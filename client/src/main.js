0a26cda817c014bd3659184684387a30f44ff310
mrinalini-m <mrinalini.manandhar@gmail.com>
2020-03-04 09:46:07 -0800

    Add gtag

--- client/src/main.js
+++ client/src/main.js
@@ -2,10 +2,15 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 
 import App from '@/App.vue'
 import Vue from 'vue'
+import VueGtag from 'vue-gtag'
 import VueMasonry from 'vue-masonry-css'
 import router from '@/router'
 import { store } from '@/store'
 import vuetify from '@/plugins/vuetify'
+
+Vue.use(VueGtag, {
+  config: { id: 'UA-159659222-1' }
+})
 
 Vue.config.productionTip = false
 Vue.use(VueMasonry)
